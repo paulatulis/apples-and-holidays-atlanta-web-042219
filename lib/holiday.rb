c2742e4c2de9af375844693d90e1e920900eb37e
Paula Tulis <tulis.paula@gmail.com>
2019-04-13 19:16:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,8 +39,6 @@ end
 end
 end
 
-
-
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
@@ -52,7 +50,7 @@ end
 end 
 end
 end
-end
+
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
